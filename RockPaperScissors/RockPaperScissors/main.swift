a9aa864d3231f43ee0426f96d13fe4f84e55d52f
babywalnut <anfuza@naver.com>
2021-03-03 12:56:59 +0900

    [#10] refector code

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,10 +7,9 @@
 import Foundation
 
 
-struct RockPaperScissors {
-    
-    let userAttackingTurn: String = "사용자"
-    let computerAttackingTurn: String = "컴퓨터"
+class RockPaperScissors {
+
+    var attackingTurn: String = "사용자"
     let rockPaperScissorsGameMessage: [String] = ["이겼습니다!", "졌습니다!", "비겼습니다!"]
     let mukchipaGameMessage: [String] = ["의 턴입니다", "의 승리!"]
     let rockPaperScissorsMode: Bool = true
@@ -23,6 +22,8 @@ struct RockPaperScissors {
             if gameMode { // 가위 바위 보
                 print("이겼습니다!")
                 // 묵찌빠 게임 시작 (사용자 턴) !gameMode
+                attackingTurn = "사용자"
+                startGame(on: mukchipaMode)
             }
             else{ // 묵찌빠
                 // 묵찌빠 게임 playGame(userChoice, gameMode)
@@ -31,24 +32,30 @@ struct RockPaperScissors {
             if gameMode{
                 print("졌습니다!")
                 // 묵찌빠 게임 시작 (컴퓨터 턴)
+                attackingTurn = "컴퓨터"
+                startGame(on: mukchipaMode)
             }
         } else {
-            print("비겼습니다!")
-            startGame()
+            if gameMode {
+                print("비겼습니다!")
+                startGame(on: gameMode)
+            } else {
+                print(attackingTurn+mukchipaGameMessage[1])
+            }
         }
     }
     
-    private func playMukchipaGame(with attackerTurn: String) {
-        // [~ 턴] 묵 찌 빠 ~ 입력 받기
-        // 잘못된 입력일 때
-            // 턴을 바꿔서 묵찌빠 게임 다시 진행
-        // 제대로 된 입력일 때
-            //
-    }
+//    private func playMukchipaGame(with attackerTurn: String) {
+//        // [~ 턴] 묵 찌 빠 ~ 입력 받기
+//        // 잘못된 입력일 때
+//            // 턴을 바꿔서 묵찌빠 게임 다시 진행
+//        // 제대로 된 입력일 때
+//            //
+//    }
     
-    private func decideGameOption(with convertedUserSelection: Int) {
+    private func decideGameOption(with convertedUserSelection: Int, on gameMode: Bool) {
         if (1...3).contains(convertedUserSelection) {
-            playGame(with: convertedUserSelection, on: rockPaperScissorsMode)
+            playGame(with: convertedUserSelection, on: gameMode)
         }
     }
     
@@ -70,27 +77,38 @@ struct RockPaperScissors {
         }
     }
     
-    private func restartGame() {
+    private func restartGame(on gameMode: Bool) {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        startGame()
+        if !gameMode {
+            if self.attackingTurn == "사용자" {
+                self.attackingTurn = "컴퓨터"
+            } else {
+                self.attackingTurn = "사용자"
+            }
+        }
+        startGame(on: gameMode)
     }
     
-    private func getUserSelection() {
+    private func getUserSelection(on gameMode: Bool) {
         if let userSelection = readLine() {
             if checkValidation(userSelection) {
                 let convertedUserSelection: Int = convert(userSelection)
-                decideGameOption(with: convertedUserSelection)
+                decideGameOption(with: convertedUserSelection, on: gameMode)
             } else {
-                restartGame()
+                restartGame(on: gameMode)
             }
         }
         }
     
-    func startGame() {
-        print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator: "")
-        getUserSelection()
+    func startGame(on gameMode: Bool) {
+        if gameMode {
+            print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator: "")
+        } else {
+            print("묵(1), 찌(2), 빠(3)! <종료:0> : ", terminator: "")
+        }
+        getUserSelection(on: gameMode)
     }
 }
 
 let newGame = RockPaperScissors()
-newGame.startGame()
+newGame.startGame(on: newGame.rockPaperScissorsMode)
